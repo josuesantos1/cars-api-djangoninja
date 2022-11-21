9c6588ffa0d8056f061655608bcc5be5881aed86
Josué Santos <josue.santos.dev@gmail.com>
2022-11-21 13:31:36 -0300

    store:  router config

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
 from cars.views import CarsView
+from users.views import UsersView
+
+api.add_router('/cars/', CarsView.router)
+api.add_router('/users/', UsersView.router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cars/', CarsView.App.urls),
+    path('api/', api.urls),
 ]
