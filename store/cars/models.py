c795ec9a013dca69c670cf8bf2247a9454f7850f
Josué Santos <josue.santos.dev@gmail.com>
2022-11-21 12:27:41 -0300

    model cars

--- store/cars/models.py
+++ store/cars/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class Cars(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
+
+    title = models.CharField(
+        max_length=256,
+    )
+
+    model = models.CharField(
+        max_length=64
+    )
+
+    price = models.FloatField(
+        blank=False,
+        null=False
+    )
+
+    isPublic = models.BooleanField(
+        default=True
+    )
+
+    # TODO: use reference with user
