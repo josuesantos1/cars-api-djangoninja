20b54128bfbebdc79ed303ea1268cfed8709b61d
Josué Santos <josue.santos.dev@gmail.com>
2022-11-21 13:31:46 -0300

    user: models

--- store/users/models.py
+++ store/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class User(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+    )
+
+    email = models.CharField(
+        max_length=250,
+        blank=False,
+        null=False
+    )
+
+    password = models.TextField(
+        null=False,
+        blank=False
+    )
